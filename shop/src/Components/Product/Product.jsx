2776757beebb9ed9c99c026d90dad561f79c5ebb
wlsghks1218 <kimkacang@gmail.com>
2024-11-19 09:37:15 +0900

    24.11.19 09.37 commit

--- shop/src/Components/Product/Product.jsx
+++ shop/src/Components/Product/Product.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import formatKoreanCurrency from '../../util/display/display';
 
 const Item = styled.div`
     color: black;
-    text-align: center;
     margin: 30px;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 5px 5px 5px 5px #e8e4e4;
+    text-align: right;
+
+    h4{
+        margin-top:10px;
+        font-size:1.3em;
+        font-weight:bold;
+    }
+    p{
+        font-size:1em;
+        padding: 0;
+        margin: 0;
+    }
+    span{
+        font-size:1.2em;
+        font-weight: 500;
+    }
 `;
 
 const Product = ({data}) => {
     console.log(data);
     return (
         <Item>
-            <img src={"/images/product/" + data.main} alt="메인이미지" width='100%'/>
+            {/* <img src={"/images/product/" + data.main} alt="메인이미지" width='100%'/> */}
+            <img src={process.env.PUBLIC_URL + `/images/product/${data.main}`} alt="메인이미지" width='100%'/>
             <h4>{data.title}</h4>
-            {/* <p>{id}</p> */}
             <p>{data.content}</p>
-            <span>{data.price}</span>
-            {/* {main} */}
-            {/* <img src={"/images/product/" + detail} alt="디테일이미지" /> */}
+            <span>{formatKoreanCurrency(data.price)}</span>
         </Item>
     );
 };
